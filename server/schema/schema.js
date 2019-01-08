401fef29eb3e3b12a6ffe00af383b6031f63e697
Shehryar Assad <xshehryar@gmail.com>
2019-01-07 22:19:38 -0500

    Add product model and root queries

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -1,1 +1,51 @@
 const graphql = require("graphql");
+
+const {
+  GraphQLObjectType,
+  GraphQLString,
+  GraphQLFloat,
+  GraphQLInt,
+  GraphQLSchema
+} = graphql;
+
+const ProductType = new GraphQLObjectType({
+  name: "Product",
+  fields: () => ({
+    id: {
+      type: GraphQLString
+    },
+    title: {
+      type: GraphQLString
+    },
+    price: {
+      type: GraphQLFloat
+    },
+    inventory_count: {
+      type: GraphQLInt
+    }
+  })
+});
+
+const RootQuery = new GraphQLObjectType({
+  name: "RootQueryType",
+  fields: {
+    product: {
+      type: ProductType,
+      args: {
+        id: {
+          type: GraphQLString
+        },
+        resolve(parent, args) {
+          // code to get data from db
+        }
+      }
+    }
+  }
+});
+
+// const ShoppingCartType = new GraphQLObjectType({
+//   name: "Shopping Cart",
+//   fields: {() => ({})}
+// });
+
+module.exports = new GraphQLSchema(RootQuery);
