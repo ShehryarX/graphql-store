d36544c76ce4ec44090093f3144dec93559fa946
Shehryar Assad <xshehryar@gmail.com>
2019-01-14 01:04:38 -0500

    Add inventory security and checkout option

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -153,6 +153,117 @@ const Mutation = new GraphQLObjectType({
         });
         return shoppingCart.save();
       }
+    },
+    addProductToShoppingCart: {
+      type: ShoppingCartType,
+      args: {
+        productId: { type: GraphQLNonNull(GraphQLID) },
+        shoppingCartId: { type: GraphQLNonNull(GraphQLID) }
+      },
+      resolve(parent, args) {
+        const { productId, shoppingCartId } = args;
+        return new Promise((resolve, reject) => {
+          Product.findById(productId).then(product => {
+            if (product) {
+              ShoppingCart.findById(shoppingCartId).then(shoppingCart => {
+                let deductions = 0;
+
+                // count how many products are in cart
+                for (let i = 0; i < shoppingCart.products.length; i++) {
+                  if (shoppingCart.products[i].id === productId) ++deductions;
+                }
+
+                if (product.inventoryCount - deductions <= 0) {
+                  return reject("Product inventory has ran out");
+                }
+
+                if (shoppingCart) {
+                  shoppingCart.numberOfItems++;
+                  shoppingCart.totalPrice += product.price;
+                  shoppingCart.products.push(productId);
+
+                  const { numberOfItems, totalPrice, products } = shoppingCart;
+
+                  shoppingCart
+                    .update({
+                      numberOfItems,
+                      totalPrice,
+                      products
+                    })
+                    .then(res => resolve(shoppingCart));
+                  return resolve(shoppingCart);
+                } else {
+                  return reject("Shopping cart not found");
+                }
+              });
+            } else {
+              return reject("Product not found");
+            }
+          });
+        });
+      }
+    },
+    checkoutShoppingCart: {
+      type: ShoppingCartType,
+      args: {
+        shoppingCartId: { type: GraphQLNonNull(GraphQLID) }
+      },
+      resolve(parent, args) {
+        const { shoppingCartId } = args;
+
+        return new Promise((resolve, reject) => {
+          ShoppingCart.findById(shoppingCartId).then(shoppingCart => {
+            if (shoppingCart) {
+              let promises = [];
+
+              let productInfo = {};
+
+              shoppingCart.products.forEach(productId => {
+                const { _id } = productId;
+                if (!productInfo[_id]) {
+                  productInfo[_id] = 0;
+                }
+                productInfo[_id]++;
+              });
+
+              let keys = Object.keys(productInfo);
+
+              for (let i = 0; i < keys.length; i++) {
+                let key = keys[i];
+                let count = productInfo[key];
+                promises.push(
+                  Product.findById(key).then(product => {
+                    const { inventoryCount } = product;
+                    if (inventoryCount >= count) {
+                      product
+                        .update({
+                          inventoryCount: inventoryCount - count
+                        })
+                        .then(() => console.log("Removed inventory"));
+                    }
+                  })
+                );
+              }
+
+              Promise.all(promises).then(() => {
+                shoppingCart.numberOfItems = 0;
+                shoppingCart.totalPrice = 0;
+                shoppingCart.products = [];
+
+                shoppingCart
+                  .update({
+                    numberOfItems: 0,
+                    totalPrice: 0,
+                    products: []
+                  })
+                  .then(res => resolve(shoppingCart));
+              });
+            } else {
+              return reject("Shopping cart not found");
+            }
+          });
+        });
+      }
     }
   }
 });
